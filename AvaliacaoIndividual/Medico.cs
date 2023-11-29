5986da52559a54e92497bcc0d1c1a3608c63ad6b
Renata CHOM <78445421+renatachom@users.noreply.github.com>
2023-11-29 20:55:14 -0300

    Update Medico.cs

--- AvaliacaoIndividual/Medico.cs
+++ AvaliacaoIndividual/Medico.cs
@@ -2,20 +2,29 @@ using System;
 
 public class Medico
 {
-    public string Nome { get; }
-    public DateTime DataNascimento { get; }
-    public string CPF { get; }
-    public string CRM { get; }
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+    private string _cpf;
+    public string CPF
+    {
+        get => _cpf;
+        set
+        {
+            if (value.Length == 11)
+                _cpf = value;
+            else
+                throw new ArgumentException("CPF deve ter 11 dígitos.");
+        }
+    }
+    public string CRM { get; set; }
+
+    public int Idade => DateTime.Now.Year - DataNascimento.Year;
 
     public Medico(string nome, DateTime dataNascimento, string cpf, string crm)
     {
         Nome = nome;
         DataNascimento = dataNascimento;
-        CPF = ValidarCPF(cpf);
-        CRM = ValidarCRM(crm);
+        CPF = cpf;
+        CRM = crm;
     }
-
-    private string ValidarCPF(string cpf) => cpf;
-
-    private string ValidarCRM(string crm) => crm;
 }
