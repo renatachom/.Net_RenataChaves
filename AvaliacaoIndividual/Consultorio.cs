dadbf150cce65e9ac322886b879d39dfc10bb039
Renata CHOM <78445421+renatachom@users.noreply.github.com>
2023-11-29 20:55:42 -0300

    Update Consultorio.cs

--- AvaliacaoIndividual/Consultorio.cs
+++ AvaliacaoIndividual/Consultorio.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,24 +7,52 @@ public class Consultorio
     private List<Medico> medicos;
     private List<Paciente> pacientes;
 
-    public Consultorio(List<Medico> medicos, List<Paciente> pacientes)
+    public Consultorio()
     {
-        this.medicos = medicos;
-        this.pacientes = pacientes;
+        medicos = new List<Medico>();
+        pacientes = new List<Paciente>();
     }
 
-    public IEnumerable<Medico> ObterMedicosPorIdade(int idadeMinima, int idadeMaxima)
+    public void AdicionarMedico(Medico medico)
     {
-        var dataAtual = DateTime.Now;
-        return medicos.Where(m => (dataAtual - m.DataNascimento).Days / 365 >= idadeMinima
-                                && (dataAtual - m.DataNascimento).Days / 365 <= idadeMaxima);
+        if (!medicos.Any(m => m.CPF == medico.CPF) && !medicos.Any(m => m.CRM == medico.CRM))
+        {
+            medicos.Add(medico);
+            Console.WriteLine($"Médico {medico.Nome} adicionado com sucesso.");
+        }
+        else
+        {
+            Console.WriteLine("CPF ou CRM já existente para um médico.");
+        }
     }
 
-    public IEnumerable<Paciente> ObterPacientesPorIdade(int idadeMinima, int idadeMaxima)
+    public void AdicionarPaciente(Paciente paciente)
     {
-        var dataAtual = DateTime.Now;
-        return pacientes.Where(p => (dataAtual - p.DataNascimento).Days / 365 >= idadeMinima
-                                  && (dataAtual - p.DataNascimento).Days / 365 <= idadeMaxima);
+        if (!pacientes.Any(p => p.CPF == paciente.CPF))
+        {
+            pacientes.Add(paciente);
+            Console.WriteLine($"Paciente {paciente.Nome} adicionado com sucesso.");
+        }
+        else
+        {
+            Console.WriteLine("CPF já existente para um paciente.");
+        }
     }
 
+    public IEnumerable<Medico> ObterMedicosComIdadeEntre(int idadeMinima, int idadeMaxima)
+    {
+        return medicos.Where(m => m.Idade >= idadeMinima && m.Idade <= idadeMaxima);
+    }
+
+    public IEnumerable<Paciente> ObterPacientesComIdadeEntre(int idadeMinima, int idadeMaxima)
+    {
+        return pacientes.Where(p => p.Idade >= idadeMinima && p.Idade <= idadeMaxima);
+    }
+
+    public IEnumerable<Paciente> ObterPacientesPorSexo(string sexo)
+    {
+        return pacientes.Where(p => p.Sexo.Equals(sexo, StringComparison.OrdinalIgnoreCase));
+    }
+
+    
 }
